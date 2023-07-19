df0ca746a5aec05e6b5703aac87c76e34b137c5c
MedAmine <med.amine.mzk@horizon-tech.tn>
2023-07-19 13:38:28 +0100

    impl logback

--- src/main/java/com/mycompany/propertymanagement/exception/CustomExceptionHandler.java
+++ src/main/java/com/mycompany/propertymanagement/exception/CustomExceptionHandler.java
@@ -1,18 +1,39 @@
 package com.mycompany.propertymanagement.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ErrorModel>> handleFieldValidation(MethodArgumentNotValidException manv){
+        List<ErrorModel> errorModelList= new ArrayList<>();
+        List<FieldError> fieldErrorList=manv.getBindingResult().getFieldErrors();
+        for(FieldError fe : fieldErrorList){
+            logger.info("Inside Field Validation: info : {} - {}",fe.getField(),fe.getDefaultMessage());
+            logger.debug("Inside Field Validation: debug : {} - {}",fe.getField(),fe.getDefaultMessage());
+            errorModelList.add(new ErrorModel(fe.getField(),fe.getDefaultMessage()));
+        }
+        return new ResponseEntity<List<ErrorModel>>(errorModelList, HttpStatus.BAD_REQUEST);
+    }
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<List<ErrorModel>> handleBusinessException(BusinessException businessException){
-        System.out.println("BusinessException is thrown ! ");
+        for (ErrorModel er : businessException.getErrors()){
+            logger.warn("BusinessException is thrown: Warn : {} - {}",er.getCode(),er.getMessage());
+            logger.error("BusinessException is thrown: Error : {} - {}",er.getCode(),er.getMessage());
+        }
         return new ResponseEntity<List<ErrorModel>>(businessException.getErrors(), HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/mycompany/propertymanagement/model/UserDTO.java
+++ src/main/java/com/mycompany/propertymanagement/model/UserDTO.java
@@ -3,6 +3,10 @@ package com.mycompany.propertymanagement.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,7 +17,13 @@ import lombok.Setter;
 public class UserDTO {
     private Long id;
     private String ownerName;
+    @NotNull(message = "Owner Email is mandatory !")
+    @NotEmpty(message = "Owner Email cannot be empty !")
+    @Size(min = 3,max = 50,message = "Owner Emeil should be between 1 and 50 characters")
+    @Email(message = "Owner Email is not valid !")
     private String ownerEmail;
     private String phone;
+    @NotNull(message = "Password is mandatory !")
+    @NotEmpty(message = "Password cannot be empty !")
     private String password;
 }
